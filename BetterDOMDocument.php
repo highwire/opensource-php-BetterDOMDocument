9d807979dbd0a6eff2246e50dd89fb91f1ac4154
Patrick Hayes <patrick.d.hayes@gmail.com>
2012-09-13 09:30:10 -0700

    Initial Commit

---
+++ BetterDOMDocument.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * Highwire Better DOM Document
+ *
+ * Copyright (c) 2010-2011 Board of Trustees, Leland Stanford Jr. University
+ * This software is open-source licensed under the GNU Public License Version 2 or later
+ * The full license is available in the LICENSE.TXT file at the root of this repository
+ */
+
+class BetterDOMDocument extends DOMDocument {
+
+  private $ns = array();
+
+  function __construct($xml = FALSE) {
+    parent::__construct();
+    if(is_object($xml)){
+      $this->appendChild($this->importNode($xml, true));
+    }
+
+    if (is_string($xml)) {
+      @$this->loadXML($xml);
+    }
+  }
+
+  // $raw should be FALSE, 'full', or 'inner'
+  function getArray($raw = FALSE, $node = NULL) {
+    $array = false;
+
+    if ($node) {
+      if ($raw == 'full') {
+        $array['#raw'] = $this->saveXML($node);
+      }
+      if ($raw == 'inner') {
+        $pattern = "/<".preg_quote($node->nodeName)."\b[^>]*>(.*)<\/".preg_quote($node->nodeName).">/s";
+        $matches = array();
+        preg_match($pattern, $this->saveXML($node), $matches);
+        $array['#raw'] = $matches[1];
+      }
+      if ($node->hasAttributes()) {
+        foreach ($node->attributes as $attr) {
+          $array['@'.$attr->nodeName] = $attr->nodeValue;
+        }
+      }
+
+      if ($node->hasChildNodes()) {
+        if ($node->childNodes->length == 1 && $node->firstChild->nodeType == XML_TEXT_NODE) {
+          $array['#text'] = $node->firstChild->nodeValue;
+        }
+        else {
+          foreach ($node->childNodes as $childNode) {
+            if ($childNode->nodeType == XML_ELEMENT_NODE) {
+              $array[$childNode->nodeName][] = $this->getArray($raw, $childNode);
+            }
+          }
+        }
+      }
+    }
+    // Else no node was passed, which means we are processing the entire domDocument
+    else {
+      foreach ($this->childNodes as $childNode) {
+        if ($childNode->nodeType == XML_ELEMENT_NODE) {
+          $array[$childNode->nodeName][] = $this->getArray($raw, $childNode);
+        }
+      }
+    }
+
+    return $array;
+  }
+
+  function createElementFromXML($xml) {
+    $dom = new BetterDOMDocument($xml);
+    if (!$dom->documentElement) {
+      highwire_system_message('BetterDomDocument Error: Invalid XML: ' . $xml, 'error');
+    }
+    $element = $dom->documentElement;
+    return $this->importNode($element, true);
+  }
+
+  function query($xpath, $contextnode = NULL) {
+    $xob = new DOMXPath($this);
+
+    // Register the namespaces
+    foreach ($this->ns as $namespace => $url) {
+      $xob->registerNamespace($namespace, $url);
+    }
+
+    // DOMDocument is a piece of shit when it comes to namespaces
+    // Instead of passing the context node, hack the xpath query to manually construct context using xpath
+    if ($contextnode) {
+      $ns = $contextnode->namespaceURI;
+      $lookup = array_flip($this->ns);
+      $prefix = $lookup[$ns];
+      $prepend = str_replace('/', '/'. $prefix . ':', $contextnode->getNodePath());
+      return $xob->query($prepend . $xpath);
+    }
+    else {
+      return $xob->query($xpath);
+    }
+  }
+
+  function query_single($xpath, $contextnode = NULL) {
+    $result = $this->query($xpath, $contextnode);
+    if ($result->length) {
+      return $result->item(0);
+    }
+    else {
+      return NULL;
+    }
+  }
+
+  function registerNamespace($namespace, $url) {
+    $this->ns[$namespace] = $url;
+  }
+
+  function replace(&$node, $replace) {
+    if (is_string($replace)) {
+      $replace = $this->createElementFromXML($replace);
+    }
+    $node->parentNode->replaceChild($replace, $node);
+    $node = $replace;
+  }
+
+  function out($contextnode = NULL) {
+    if (!$contextnode) {
+      $contextnode = $this->firstChild;
+    }
+    return $this->saveXML($contextnode, LIBXML_NOEMPTYTAG);
+  }
+
+}
+
---
+++ README.md
@@ -0,0 +1,28 @@
+A nice handy wrapper for PHP's built in DOMDocument. Provides a bunch of nice little shortcuts. 
+
+```php
+<?php
+
+// We can load a new BetterDOMDocument from either a string or a DOMNode object
+$dom = new BetterDOMDocument($xmlstring);
+
+// It's easy to output the entire documenent as an array, which is sometimes easier to work with in PHP
+$array = dom->getArray();
+
+// It's easy to query too!
+$node_list = $dom->query('//xpath/to/node', $optional_context_node);
+
+// If you know you're only going to find a single DOMNode, you can use a query_single
+$dom_node = $dom->querySingle('//xpath/to/node');
+
+// If you need to register a namespace, thats a snap
+$dom->registerNamespace('kml','http://www.opengis.net/kml/2.2');
+
+// Swapping out DOMNodes is really easy
+$dom->replace($DomNode, $replacementNode);
+
+// It's easy to output the XML
+$xml = $dom->out();
+
+// Or just output a single DOMNode
+$xml = $dom->out($DOMNode);
