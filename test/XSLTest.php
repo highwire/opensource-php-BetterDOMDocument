de850d0408182bd551dfd2b27c2252b1f0252bdd
phayes <patrick.d.hayes@gmail.com>
2015-03-26 12:34:00 -0700

    tests now pass

--- test/XSLTest.php
+++ test/XSLTest.php
@@ -4,14 +4,15 @@ include_once('BetterDOMDocument.php');
 
 class AsHTMLTest extends PHPUnit_Framework_TestCase {
     private $xml = '<nlm:contrib xmlns:nlm="http://schema.highwire.org/NLM/Journal" nlm:contrib-type="author"><nlm:string-name nlm:test="teststring">Patrick Douglas Hayes</nlm:string-name></nlm:contrib>';
-    private $html = ''
+    private $html = '<span class="nlm-contrib" data-nlm-contrib-type="author"><span class="nlm-string-name" data-nlm-test="teststring">Patrick Douglas Hayes</span></span>';
+    private $contexthtml = '<span class="nlm-string-name" data-nlm-test="teststring">Patrick Douglas Hayes</span>';
 
     public function testAsHTML() {
         $dom = new BetterDOMDocument($this->xml);
-        print $this->asHTML();
+        $this->assertEquals($this->html, $dom->asHTML());
     }
     public function testAsHTMLContext() {
         $dom = new BetterDOMDocument($this->xml);
-        print $this->asHTML('//nlm:string-name');
+         $this->assertEquals($this->contexthtml, $dom->asHTML('//nlm:string-name'));
     }
 }
