18a1253460259cc66a4a75d5e53f8de5ac22432d
phayes <patrick.d.hayes@gmail.com>
2017-04-27 18:59:55 -0400

    Updating tests to use latest phpunit

--- tests/XPathTest.php
+++ tests/XPathTest.php
@@ -2,40 +2,17 @@
 
 use BetterDOMDocument\DOMDoc;
 
-class PrefixTest extends PHPUnit_Framework_TestCase {
-    private $xml = '<nlm:contrib xmlns:nlm="http://schema.highwire.org/NLM/Journal" nlm:contrib-type="author"><nlm:string-name nlm:test="teststring">Patrick Douglas Hayes</nlm:string-name></nlm:contrib>';
+class XPathTest extends PHPUnit_Framework_TestCase {
+    private $prefix_xml = '<nlm:contrib xmlns:nlm="http://schema.highwire.org/NLM/Journal" nlm:contrib-type="author"><nlm:string-name nlm:test="teststring">Patrick Douglas Hayes</nlm:string-name></nlm:contrib>';
 
-    public function testAttr() {
-        $dom = new DOMDoc($this->xml);
-        $this->assertEquals("teststring", $dom->xpathSingle('//@nlm:test')->nodeValue);
-    }
-    public function testAttrContext() {
-        $dom = new DOMDoc($this->xml);
-        $this->assertEquals("teststring", $dom->xpathSingle('@nlm:test', "//nlm:string-name")->nodeValue);
-    }
-}
-
-class MultiPrefixTest extends PHPUnit_Framework_TestCase {
-    private $xml = '<?xml version="1.0"?><!-- This is a comment -->
+    private $multi_prefix_xml = '<?xml version="1.0"?><!-- This is a comment -->
                     <atom:author test="123" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:nlm="http://schema.highwire.org/NLM/Journal" xmlns:hwp="http://schema.highwire.org/Journal" nlm:contrib-type="author">
                       <atom:name>Shubhayan Sanatani</atom:name>
                       <nlm:name name-style="western" hwp:sortable="Sanatani Shubhayan"><nlm:surname>Sanatani</nlm:surname><nlm:given-names>Shubhayan</nlm:given-names></nlm:name>
                       <nlm:name name-style="eastern" hwp:sortable="Li Xu"><nlm:surname>Li</nlm:surname><nlm:given-names>Xu</nlm:given-names></nlm:name>
                     </atom:author>';
-
-    public function testAttr() {
-        $dom = new DOMDoc($this->xml);
-        $this->assertEquals("Li Xu", $dom->xpathSingle('//nlm:name[@name-style="eastern"]/@hwp:sortable')->nodeValue);
-    }
-    public function testContext() {
-        $dom = new DOMDoc($this->xml);
-        $this->assertEquals("Sanatani", $dom->xpathSingle('.//nlm:surname', "//nlm:name[@name-style='western']")->nodeValue);
-    }
-}
-
-
-class HWSearchTest extends PHPUnit_Framework_TestCase {
-    private $xml = '<?xml version="1.0" encoding="UTF-8"?>
+    
+    private $hwsearch_xml = '<?xml version="1.0" encoding="UTF-8"?>
                     <hwsearch:results xmlns:hwsearch="http://schema.highwire.org/Search"
                        query-uuid="d7a5cf55-b5ea-4031-8014-3aa66600a4ba" total-result-count="10405356" first-result="1">
                         <hwsearch:result index="1">
@@ -57,9 +34,28 @@ class HWSearchTest extends PHPUnit_Framework_TestCase {
                         </hwsearch:result>
                     </hwsearch:results>';
 
+    public function testPrefixAttr() {
+        $dom = new DOMDoc($this->prefix_xml);
+        $this->assertEquals("teststring", $dom->xpathSingle('//@nlm:test')->nodeValue);
+    }
+    public function testPrefixAttrContext() {
+        $dom = new DOMDoc($this->prefix_xml);
+        $this->assertEquals("teststring", $dom->xpathSingle('@nlm:test', "//nlm:string-name")->nodeValue);
+    }
 
-    public function testFirstTitle() {
-        $dom = new DOMDoc($this->xml);
+    public function testMultiPrefixAttr() {
+        $dom = new DOMDoc($this->multi_prefix_xml);
+        $this->assertEquals("Li Xu", $dom->xpathSingle('//nlm:name[@name-style="eastern"]/@hwp:sortable')->nodeValue);
+    }
+
+    public function testMutiPrefixContext() {
+        $dom = new DOMDoc($this->multi_prefix_xml);
+        $this->assertEquals("Sanatani", $dom->xpathSingle('.//nlm:surname', "//nlm:name[@name-style='western']")->nodeValue);
+    }
+
+    public function testHWSearch() {
+        $dom = new DOMDoc($this->hwsearch_xml);
         $this->assertEquals("Test Title", $dom->xpathSingle('.//hwsearch:highwire-title', "//hwsearch:result[@index='1']")->nodeValue);
     }
+
 }
