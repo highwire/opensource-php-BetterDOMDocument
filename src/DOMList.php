e8c1534e816f4b27c177c05016f80dcce46325bc
suhasa-holla <105205832+suhasa-holla@users.noreply.github.com>
2023-07-26 12:58:40 +0530

    Prototype updated as per D9 standard
    Prototype updated as per D9 standard

--- src/DOMList.php
+++ src/DOMList.php
@@ -33,23 +33,23 @@ class DOMList implements \Countable, \Iterator {
     }
   }
 
-  public function rewind() {
+  public function rewind(): void {
     $this->position = 0;
   }
 
-  public function current() {
+  public function current(): mixed {
     return $this->array[$this->position];
   }
 
-  public function key() {
+  public function key(): mixed {
     return $this->position;
   }
 
-  public function next() {
+  public function next(): void {
     ++$this->position;
   }
 
-  public function valid() {
+  public function valid(): bool {
     return isset($this->array[$this->position]);
   }
   
